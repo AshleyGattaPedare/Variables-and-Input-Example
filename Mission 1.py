639ee19799d66f2131f8a55ad0e0b6046be011de
AshleyGattaPedare <161656406+AshleyGattaPedare@users.noreply.github.com>
2024-07-19 14:50:17 +0930

    Update and rename Base Code.py to Mission 1.py

---
+++ Mission 1.py
@@ -0,0 +1,22 @@
+#First things first, you need to create a basic menu system scientists can store information about their current space mission.
+
+#Step 1: A print statement has been used to welcome the scientist to the menu system.
+#Use a print statement underneath which reads "Please provide details of the current mission."
+
+print("Hello earthling scientist!")
+
+
+#Step 2: Below, the lead astronaut's name will be stored as a variable called 'lead_astronaut.'
+#Use the variable named 'mission_name' to store "Mars Rover Mission" and 'launch_year' to store "2035."
+
+
+lead_astronaut = input("Enter the lead astronaut's name: ")
+mission_name = 
+launch_year = 
+
+#Step 3: Fill in the variable names between the strings provided to create a mission brief. Make sure to use a print statement to view the mission brief.
+
+mission_brief = print(         + "will commence in " +         + " lead by " +         )
+
+
+#Step 4: The project gets delayed by 3 years because *beaucracy*. Go back to step 2 and add 3 to the launch year. Run the code to check if it works.
