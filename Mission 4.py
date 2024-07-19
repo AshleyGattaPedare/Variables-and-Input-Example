082c95f7107d68733039fb4f81529d78b708627b
AshleyGattaPedare <161656406+AshleyGattaPedare@users.noreply.github.com>
2024-07-19 15:39:58 +0930

    Create Mission 4.py

---
+++ Mission 4.py
@@ -0,0 +1,29 @@
+#Your supervisor is extremely impressed with your work so far so he puts you on the toughest project yet. 
+#Using the evidence provided by the Mars Rover Perserverance, you are going to produce a report which indicates 
+   # whether the environmental conditions were potentially conducive to life and link it to the Mars Rover project objectives.
+
+#Step 1: Collect the geological data by asking the rover if there is any sedimentary rocks (Yes or No), river chanels (Yes or No) and what types of minerals were found.
+
+sedimentary_rocks = 
+river_channels = 
+mineral_type = 
+
+
+#Step 2: Set up a boolean (True or Talse) to help us later assess if water is or is not present 
+
+water_present = 
+
+
+#Step 3: Use if statements to change water_present to True if sedimentary_rocks or river_channels were found.
+
+if 
+  water_present = True 
+  print("Water is present. Life may be sustainable on planet.")
+
+
+#Step 4: If 'Clay' or 'Carbonates' were detected under mineral_type, change water_present to True and print the statement below.
+
+if 
+  
+  print("Water is present. Life may be sustainable on planet.")
+
